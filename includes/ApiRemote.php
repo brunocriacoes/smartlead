eb5e5aef1806517e54fc701d95a75897dac0e40b
MaxTecnology <maxsouza150@hotmail.com>
2024-04-15 11:36:33 -0300

    Fix: PDO

--- includes/ApiRemote.php
+++ includes/ApiRemote.php
@@ -25,9 +25,18 @@ class ApiRemote
         return $produtos;
     }
 
+    public function getIdCategoryBySlug($slug)
+    {
+        $query = "SELECT id FROM categories WHERE slug = ?";
+        $categoria = $this->db->select($query, array($slug));
+        return $categoria[0]['id'];
+    }
+
     public function getProductsByCategory($categoryId)
     {
-        $query = "SELECT p.id id, p.name name, p.slug slug, p.cod cod, p.specifications specifications, p.internal_part internal_part, p.external_part external_part FROM products p INNER JOIN category_product cp ON p.id = cp.product_id WHERE cp.slug = ?";
+        $id_category = $this->getIdCategoryBySlug($categoryId);
+        die($id_category);
+        $query = "SELECT p.id id, p.name name, p.slug slug, p.cod cod, p.specifications specifications, p.internal_part internal_part, p.external_part external_part FROM products p INNER JOIN category_product cp ON p.id = cp.product_id WHERE cp.category_id = ?";
         $produtos = $this->db->select($query, array($categoryId));
         foreach ($produtos as &$produto) {
             $query = "SELECT id, path FROM product_photos WHERE product_id = ?";
