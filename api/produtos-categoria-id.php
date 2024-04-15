6399a398e96d8cd4928a3da584c0c81c9dfa6fbb
MaxTecnology <maxsouza150@hotmail.com>
2024-04-15 11:39:51 -0300

    Fix: PDO

--- api/produtos-categoria-id.php
+++ api/produtos-categoria-id.php
@@ -5,7 +5,7 @@ function custom_api_produtos_categoria_id()
     register_rest_route('smartlead-api/v1', '/produtos-categoria-id', array(
         'methods'  => 'GET',
         'callback' => 'custom_api_get_produtos_categoria_id',
-        'args' => array(),
+
     ));
 }
 add_action('rest_api_init', 'custom_api_produtos_categoria_id');
